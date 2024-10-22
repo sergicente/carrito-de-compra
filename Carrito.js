16f8fad376bdc4374b9506a72cc150e6ae43e492
sergicente <sergicente@icloud.com>
2024-10-22 12:58:31 +0200

    codigo optimizado

--- Carrito.js
+++ Carrito.js
@@ -8,23 +8,23 @@ class Carrito {
         let existe = false;
 
         // Buscamos si está en el carrito y si está incrementamos cantidad en 1 unidad
-        for (let i = 0; i < this.productos.length; i++) {
-            if (this.productos[i].SKU === producto.SKU) {
+        this.productos.forEach((p) => {
+            if (p.SKU === producto.SKU) {
                 existe = true;
-                this.productos[i].cantidad++;
-                this.productos[i].total = this.productos[i].price * this.productos[i].cantidad;
-                break;
+                p.cantidad++;
+                p.total = p.price * p.cantidad;
+                return; // Para simular el break y salir del forEach.
             }
-        }
+        });
 
         // Si no está en el carrito lo añadimos con cantidad 1
         if (!existe) {
             this.productos.push({
                 title: producto.title,
                 SKU: producto.SKU,
-                price: producto.price,
+                price: parseFloat(producto.price),
                 cantidad: 1,
-                total: producto.price,
+                total:  parseFloat(producto.price),
             });
         }
     }
@@ -36,44 +36,44 @@ class Carrito {
 
     // Método para sumar una unidad
     sumarUnidad(sku) {
-        for (let i = 0; i < this.productos.length; i++) {
-            if (this.productos[i].SKU === sku) {
-                ++this.productos[i].cantidad;
-                this.productos[i].total = this.productos[i].price * this.productos[i].cantidad;
-                break;
+        this.productos.forEach((p) => {
+            if (p.SKU === sku) {
+                ++p.cantidad;
+                p.total = p.price * p.cantidad;
+                return; // Para simular el break y salir del forEach.
             }
-        }
+        });
     }
 
     // Método para restar una unidad
     restarUnidad(sku) {
-        for (let i = 0; i < this.productos.length; i++) {
-            if (this.productos[i].SKU === sku) {
-                --this.productos[i].cantidad;
-                this.productos[i].total = this.productos[i].price * this.productos[i].cantidad;
-                break;
+        this.productos.forEach((p)=>{
+            if (p.SKU === sku) {
+                --p.cantidad;
+                p.total = p.price * p.cantidad;
+                return;
             }
-        }
+        })
+
     }
 
     // Método para sacar un producto del carrito
     eliminarProducto(sku) {
         let nuevoArrayProductos = [];
-        for (let i = 0; i < this.productos.length; i++) {
-            if (this.productos[i].SKU !== sku) {
-                nuevoArrayProductos.push(this.productos[i]);
+        this.productos.forEach((p) => {
+            if (p.SKU !== sku) {
+                nuevoArrayProductos.push(p);
             }
-        }
+        });
         this.productos = nuevoArrayProductos;
     }
 
     // Método para calcular el total
     calcularSubtotal() {
         let subtotal = 0;
-        for (let i = 0; i < this.productos.length; i++) {
-            subtotal += this.productos[i].price * this.productos[i].cantidad;
-        }
-
+        this.productos.forEach((p) => {
+            subtotal += p.price * p.cantidad;
+        });
         return subtotal;
     }
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import Carrito from "./Carrito.js";
 
 const tablaProductos = document.querySelector("#productos table");
+const listadoProductos = document.querySelector("#productos")
 const tablaCarrito = document.querySelector("#carrito table");
 const subtotal = document.querySelector("#subtotal");
 const botonLocalStorage = document.querySelector("#btnLocalStorage");
@@ -9,164 +10,164 @@ const total = document.querySelector("#total");
 const carrito = new Carrito();
 let catalogoProductos = [];
 
-document.addEventListener('DOMContentLoaded', function(event){
-    botonLocalStorage.addEventListener("click", function(){
-        if(botonLocalStorage.textContent === "Activado"){
-            botonLocalStorage.textContent = "Desactivado";
-            botonLocalStorage.classList.remove('activado');
-        }else{
-            botonLocalStorage.textContent = "Activado";
-            botonLocalStorage.classList.add('activado');
-            guardarEnLocalStorage();
-        }
-        localStorage.setItem("botonTexto", botonLocalStorage.textContent);
-    })
-    
-    
-    
+document.addEventListener("DOMContentLoaded", function (event) {
     // Obtenemos los productos desde la API utilizando al forma moderna con arrow functions
     fetch("https://jsonblob.com/api/jsonBlob/1294949121184882688")
         .then((response) => response.json())
         .then((posts) => {
             catalogoProductos = posts.products;
             crearTablaProductos(catalogoProductos);
+        })
+        // Añadimos también un mensaje de error que aparezca en el listado de productos
+        .catch(() => {
+            const mensajeError = document.createElement("p");
+            mensajeError.textContent = "Error al cargar los productos. Por favor, inténtalo más tarde.";
+            listadoProductos.appendChild(mensajeError);
         });
-    
+
     // Función para crear la tabla de productos
     function crearTablaProductos(productos) {
-        for (let i = 0; i < productos.length; i++) {
-            const nuevaFila = document.createElement("tr");
-            nuevaFila.innerHTML = `
-                <td>${productos[i].title}</td>
-                <td>${productos[i].SKU}</td>
-                <td>${productos[i].price} €</td>
-                <td><button class="comprar" sku="${productos[i].SKU}">Añadir a la Cesta</button></td>
-            `;
-            tablaProductos.appendChild(nuevaFila);
-        }
-    
-        // Agregamos funcionalidad a los botones de compra
-        const botonesComprar = document.querySelectorAll(".comprar");
-        botonesComprar.forEach((boton) => {
-            boton.addEventListener("click", () => {
-                agregarAlCarrito(boton.getAttribute("sku"));
+        productos.forEach((p) => {
+            const nuevaLinea = document.createElement("tr");
+            const nombre = document.createElement("td");
+            const id = document.createElement("td");
+            const precio = document.createElement("td");
+            const comprar = document.createElement("td");
+            const botonComprar = document.createElement("button");
+            nombre.innerText = p.title;
+            id.innerText = p.SKU;
+            precio.innerText = p.price;
+            botonComprar.innerHTML = `Añadir a la cesta`;
+            botonComprar.classList.add("comprar");
+            botonComprar.addEventListener("click", function () {
+                carrito.agregarProducto(p);
+                actualizarCarrito();
             });
+            comprar.append(botonComprar);
+            nuevaLinea.append(nombre, id, precio, comprar);
+            tablaProductos.appendChild(nuevaLinea);
         });
     }
-    
-    // Función para agregar producto al carrito de compra
-    function agregarAlCarrito(sku) {
-        for (let i = 0; i < catalogoProductos.length; i++) {
-            if (catalogoProductos[i].SKU === sku) {
-                carrito.agregarProducto(catalogoProductos[i]);
-                actualizarCarrito();
-            }
-        }
-    }
-    
+
     // Función para actualizar el carrito
     function actualizarCarrito() {
         subtotal.textContent = parseFloat(carrito.calcularSubtotal()).toFixed(2) + " €";
         iva.textContent = parseFloat(carrito.calcularIva()).toFixed(2) + " €";
         total.textContent = parseFloat(carrito.calcularTotal()).toFixed(2) + " €";
         const productosEnElCarrito = carrito.obtenerProductos();
-    
+
         // Añadimos la cabecera de la tabla
-        tablaCarrito.innerHTML = `
-            <tr>
-                <th>Producto</th>
-                <th>Cantidad</th>
-                <th>Precio</th>
-                <th>Total</th>
-            </tr>
-        `;
-    
+        tablaCarrito.innerHTML = null;
+        const cabecera = document.createElement("tr");
+        const thProducto = document.createElement("th");
+        thProducto.textContent = "Producto";
+        const thCantidad = document.createElement("th");
+        thCantidad.textContent = "Cantidad";
+        const thPrecio = document.createElement("th");
+        thPrecio.textContent = "Precio";
+        const thTotal = document.createElement("th");
+        thTotal.textContent = "Total";
+        cabecera.append(thProducto, thCantidad, thPrecio, thTotal);
+        tablaCarrito.append(cabecera);
+
         // Añadimos por cada artículo en el carrito una línea en la tabla
-        for (let i = 0; i < productosEnElCarrito.length; i++) {
+        productosEnElCarrito.forEach((producto) => {
+            // Producto
             const nuevaFila = document.createElement("tr");
-            nuevaFila.innerHTML = `
-                <td>${productosEnElCarrito[i].title}</td>
-                <td>
-                    <button class="botCantMenos" sku="${productosEnElCarrito[i].SKU}">-</button>
-                    ${productosEnElCarrito[i].cantidad}
-                    <button class="botCantMas" sku="${productosEnElCarrito[i].SKU}">+</button>
-                </td>
-                <td>${parseFloat(productosEnElCarrito[i].price).toFixed(2)} €</td>
-                <td>${parseFloat(productosEnElCarrito[i].total).toFixed(2)} €</td>
-            `;
-            tablaCarrito.appendChild(nuevaFila);
-        }
-    
-        // Añadir eventos a los botones "menos" y "más"
-        const botonesMas = document.querySelectorAll(".botCantMas");
-        const botonesMenos = document.querySelectorAll(".botCantMenos");
-    
-        // Añadimos la funcionalidad de sumar una unidad
-        for (let i = 0; i < botonesMas.length; i++) {
-            botonesMas[i].addEventListener("click", function () {
-                const sku = this.getAttribute("sku");
-                carrito.sumarUnidad(sku);
-                actualizarCarrito();
-            });
-        }
-        // Añadimos la funcionalidad de restar una unidad
-        for (let i = 0; i < botonesMenos.length; i++) {
-            botonesMenos[i].addEventListener("click", function () {
-                const sku = this.getAttribute("sku");
-                if (buscarProductoEnCarrito(sku).cantidad > 1) {
-                    carrito.restarUnidad(sku);
+            const tdProducto = document.createElement("td");
+            tdProducto.textContent = producto.title;
+
+            // Cantidad
+            const tdCantidad = document.createElement("td");
+            const botonMenos = document.createElement("button");
+            const botonMas = document.createElement("button");
+            const cantidad = document.createElement("span");
+            botonMenos.innerText = "-";
+            cantidad.innerText = producto.cantidad;
+            botonMas.innerText = "+";
+
+            // Añadimos los estilos
+            botonMenos.classList.add("botCantMenos");
+            botonMas.classList.add("botCantMas");
+
+            // Añadimos funcionalidad al botón de restar
+            botonMenos.addEventListener("click", function () {
+                if (producto.cantidad > 1) {
+                    carrito.restarUnidad(producto.SKU);
+                    actualizarCarrito();
                 } else {
-                    carrito.eliminarProducto(sku);
+                    carrito.eliminarProducto(producto.SKU);
+                    actualizarCarrito();
                 }
+            });
+
+            // Añadimos funcionalidad al botón de sumar
+            botonMas.addEventListener("click", function () {
+                carrito.sumarUnidad(producto.SKU);
                 actualizarCarrito();
             });
-        }
+            tdCantidad.append(botonMenos, cantidad, botonMas);
+
+            // Precio
+            const tdPrecio = document.createElement("td");
+            tdPrecio.textContent = parseFloat(producto.price).toFixed(2);
+
+            // Total
+            const tdTotal = document.createElement("td");
+            tdTotal.textContent = parseFloat(producto.total).toFixed(2);
+
+            // Añadimos todo a la nueva fila
+            nuevaFila.append(tdProducto, tdCantidad, tdPrecio, tdTotal);
+            tablaCarrito.appendChild(nuevaFila);
+        });
+
+        // Guardamos los cambios en el Local Storage
         guardarEnLocalStorage();
-    
-    
     }
-    
-    // Función para buscar un producto en el carrito
-    function buscarProductoEnCarrito(sku) {
-        for (let i = 0; i < carrito.obtenerProductos().length; i++) {
-            if (carrito.obtenerProductos()[i].SKU === sku) {
-                return carrito.obtenerProductos()[i];
-                break;
-            }
+
+    // Listener que escuchará si se activa o no el botón de guardado de datos en LocalStorage
+    botonLocalStorage.addEventListener("click", function () {
+        if (botonLocalStorage.textContent === "Activado") {
+            botonLocalStorage.textContent = "Desactivado";
+            botonLocalStorage.classList.remove("activado");
+        } else {
+            botonLocalStorage.textContent = "Activado";
+            botonLocalStorage.classList.add("activado");
+            guardarEnLocalStorage();
         }
-        return null;
-    }
-    
-    function guardarEnLocalStorage(){
+        localStorage.setItem("botonTexto", botonLocalStorage.textContent);
+    });
+
+    // Función que se encarga de guardar el carrito en el Local Storage
+    function guardarEnLocalStorage() {
         // Convertimos el carrito a una cadena JSON
         const carritoComoJSON = JSON.stringify(carrito.productos);
         // Guardar la cadena en localStorage con el nombre carrito
         localStorage.setItem("carrito", carritoComoJSON);
     }
-    
+
     // Obtenemos el carrito desde el Local Storage una vez se carga la página
     window.onload = function () {
         // Asignamos un posible carrito del localStorage a una variable
         const carritoEnJson = localStorage.getItem("carrito");
-    
+
         // Si existe la posición del botón, recuperarla y asignarla al botón
         const botonTextoGuardado = localStorage.getItem("botonTexto");
         if (botonTextoGuardado) {
             botonLocalStorage.textContent = botonTextoGuardado;
         }
-    
+
         // Actualizamos la clase del botón según su estado almacenado
         if (botonLocalStorage.textContent === "Activado") {
-            botonLocalStorage.classList.add('activado');
+            botonLocalStorage.classList.add("activado");
         } else {
-            botonLocalStorage.classList.remove('activado');
+            botonLocalStorage.classList.remove("activado");
         }
-    
+
         // Cargamos el carrito solo si el botón está activado
         if (carritoEnJson && botonLocalStorage.textContent === "Activado") {
             carrito.productos = JSON.parse(carritoEnJson);
             actualizarCarrito();
         }
     };
-})
-
+});
